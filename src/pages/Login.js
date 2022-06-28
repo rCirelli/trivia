74d58317bc0a0f031d67a127b7f5cd664effb2e8
Helio Souza <heliorr7@gmail.com>
2022-06-28 16:43:26 -0300

    requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
+import Input from '../components/Input';
 
 class Login extends Component {
+  state = {
+    name: '',
+    email: '',
+    isDisabled: true,
+  };
+
+  valitadionButton = () => {
+    const { name, email } = this.state;
+    if (!name || !email) {
+      return this.setState({ isDisabled: true });
+    }
+    return this.setState({ isDisabled: false });
+  }
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value }, this.valitadionButton());
+  }
+
   render() {
+    const { name, email, isDisabled } = this.state;
     return (
-      <h1>Hello</h1>
+      <div>
+        <Input
+          textLabel="Nome"
+          type="text"
+          id="name"
+          name="name"
+          value={ name }
+          handleChange={ this.handleChange }
+          dataTestid="input-player-name"
+        />
+        <Input
+          textLabel="Email"
+          type="text"
+          id="email"
+          name="email"
+          value={ email }
+          handleChange={ this.handleChange }
+          dataTestid="input-gravatar-email"
+        />
+        <button data-testid="btn-play" type="button" disabled={ isDisabled }>Play</button>
+      </div>
     );
   }
 }
